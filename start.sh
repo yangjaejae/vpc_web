ddbd93f41f5d54dd1389cc662b0b9756daa988d6
yangjaejae <undertakin@naver.com>
2020-05-11 11:18:31 +0900

    docker

--- start.sh
+++ start.sh
@@ -7,6 +7,7 @@ COMPOSE_FILE=docker-compose.yaml
 echo "start WEB Server"
 
 echo "Build image#############################################"
+docker rm -f $(docker ps -aqf name="${DOCKER_CONTAINER_NAME}")
 docker build -t ${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_TAG} .
 docker run -d \
            -p "8080:8080" \
@@ -14,45 +15,4 @@ docker run -d \
            -v ~/vpc_web/:/usr/src/app \
            -w "/usr/src/app" \
            ${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_TAG} \
-           "npm start"
-
-# function restartContainers() {
-#   docker rm -f $(docker ps -aqf name="${DOCKER_CONTAINER_NAME}")
-#   docker-compose -f ${COMPOSE_FILE} up -d 2>&1
-# }
-
-# function rebuildDocker() {
-#     echo "Stop and Remove current WEB Server docker container..."
-#     docker rm -f $(docker ps -aqf name="${DOCKER_CONTAINER_NAME}")
-
-#     if [[ "$(docker images -q ${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_TAG} 2> /dev/null)" != "" ]]; then
-#         echo "Remove WEB Server docker IMAGE!!.."
-#         docker rmi -f ${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_TAG}
-#     fi
-
-#     echo "Build WEB Server image.."
-#     docker build -t ${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_TAG} .
-#     docker-compose -f ${COMPOSE_FILE} up -d 2>&1
-# }
-
-
-# function askProceed () {
-#   read -p "Remove CA-Registry Server images and container to rebuild those items(Y) or just restart container(N) ? [Y/N] " ans
-#   case "$ans" in
-#     y|Y|"" )
-#       echo "Remove and rebuild docker container of WEB Server"
-#       rebuildDocker
-#     ;;
-#     n|N )
-#       echo "Restart docker container of WEB Server..."
-#       restartContainers
-#     ;;
-#     * )
-#       echo "invalid response"
-#       askProceed
-#     ;;
-#   esac
-# }
-
-
-# askProceed
+           npm start
