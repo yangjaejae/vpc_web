c124c55a1c18881988ded6e452146353718021b0
yangjaejae <undertakin@naver.com>
2020-05-11 11:28:44 +0900

    docker

--- start.sh
+++ start.sh
@@ -3,7 +3,6 @@
 DOCKER_CONTAINER_NAME="web"
 DOCKER_IMAGE_NAME="web/nodejs"
 DOCKER_IMAGE_TAG="v1.0.0"
-COMPOSE_FILE=docker-compose.yaml
 echo "start WEB Server"
 
 echo "Build image#############################################"
@@ -11,7 +10,7 @@ docker rm -f $(docker ps -aqf name="${DOCKER_CONTAINER_NAME}")
 docker build -t ${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_TAG} .
 docker run -d \
            -p "8080:8080" \
-           --name="web" \
+           --name="${DOCKER_CONTAINER_NAME}" \
            -v ~/vpc_web/:/usr/src/app \
            -w "/usr/src/app" \
            --restart=on-failure \
