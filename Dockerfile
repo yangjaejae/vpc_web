6a4b2ed43a28e227734c193a78ff276e405585c1
yangjaejae <undertakin@naver.com>
2020-05-11 11:08:24 +0900

    docker

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,11 @@
 FROM node:10.15.2
 
 # 앱 디렉터리 생성
-#ENV PATH /usr/local/bin:$PATH
-#ENV PYTHON_VERSION 2.7
 RUN mkdir -p /usr/src/app
 
-#RUN echo "Asia/Seoul" > /etc/timezone
-#RUN dpkg-reconfigure -f noninteractive tzdata
+RUN echo "Asia/Seoul" > /etc/timezone
 
+WORKDIR /usr/src/app
 CMD chmod 777 ./*
 
 # 앱 의존성 설치
--- start.sh
+++ start.sh
@@ -6,43 +6,53 @@ DOCKER_IMAGE_TAG="v1.0.0"
 COMPOSE_FILE=docker-compose.yaml
 echo "start WEB Server"
 
-function restartContainers() {
-  docker rm -f $(docker ps -aqf name="${DOCKER_CONTAINER_NAME}")
-  docker-compose -f ${COMPOSE_FILE} up -d 2>&1
-}
+echo "Build image#############################################"
+docker build -t ${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_TAG} .
+docker run -d \
+           -p "8080:8080" \
+           --name="web" \
+           -v ~/vpc_web/:/usr/src/app \
+           -w "/usr/src/app" \
+           ${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_TAG} \
+           "npm start"
 
-function rebuildDocker() {
-    echo "Stop and Remove current WEB Server docker container..."
-    docker rm -f $(docker ps -aqf name="${DOCKER_CONTAINER_NAME}")
+# function restartContainers() {
+#   docker rm -f $(docker ps -aqf name="${DOCKER_CONTAINER_NAME}")
+#   docker-compose -f ${COMPOSE_FILE} up -d 2>&1
+# }
 
-    if [[ "$(docker images -q ${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_TAG} 2> /dev/null)" != "" ]]; then
-        echo "Remove WEB Server docker IMAGE!!.."
-        docker rmi -f ${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_TAG}
-    fi
+# function rebuildDocker() {
+#     echo "Stop and Remove current WEB Server docker container..."
+#     docker rm -f $(docker ps -aqf name="${DOCKER_CONTAINER_NAME}")
 
-    echo "Build WEB Server image.."
-    docker build -t ${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_TAG} .
-    docker-compose -f ${COMPOSE_FILE} up -d 2>&1
-}
+#     if [[ "$(docker images -q ${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_TAG} 2> /dev/null)" != "" ]]; then
+#         echo "Remove WEB Server docker IMAGE!!.."
+#         docker rmi -f ${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_TAG}
+#     fi
 
-
-function askProceed () {
-  read -p "Remove CA-Registry Server images and container to rebuild those items(Y) or just restart container(N) ? [Y/N] " ans
-  case "$ans" in
-    y|Y|"" )
-      echo "Remove and rebuild docker container of WEB Server"
-      rebuildDocker
-    ;;
-    n|N )
-      echo "Restart docker container of WEB Server..."
-      restartContainers
-    ;;
-    * )
-      echo "invalid response"
-      askProceed
-    ;;
-  esac
-}
+#     echo "Build WEB Server image.."
+#     docker build -t ${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_TAG} .
+#     docker-compose -f ${COMPOSE_FILE} up -d 2>&1
+# }
 
 
-askProceed
+# function askProceed () {
+#   read -p "Remove CA-Registry Server images and container to rebuild those items(Y) or just restart container(N) ? [Y/N] " ans
+#   case "$ans" in
+#     y|Y|"" )
+#       echo "Remove and rebuild docker container of WEB Server"
+#       rebuildDocker
+#     ;;
+#     n|N )
+#       echo "Restart docker container of WEB Server..."
+#       restartContainers
+#     ;;
+#     * )
+#       echo "invalid response"
+#       askProceed
+#     ;;
+#   esac
+# }
+
+
+# askProceed
