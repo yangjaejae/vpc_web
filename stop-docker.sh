8f7f741de0fe43faa51955dab39c2c0dbe814113
yangjaejae <undertakin@naver.com>
2020-05-12 16:20:04 +0900

    appspec

--- stop-docker.sh
+++ stop-docker.sh
@@ -4,8 +4,5 @@
 pwd
 whoami
 
-cd /home/ec2-user/vpc_web
-sudo rm -rf ./*
-
 echo "Remove container#############################################"
 docker stop $(docker ps -qa) && docker rm $(docker ps -qa)
