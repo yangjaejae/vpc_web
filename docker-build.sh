aebba052b919227286f0cf961a393e2b9c4ecba6
yangjaejae <undertakin@naver.com>
2020-05-12 13:34:40 +0900

    appspec

--- docker-build.sh
+++ docker-build.sh
@@ -4,5 +4,7 @@ DOCKER_IMAGE_NAME="web/nodejs"
 DOCKER_IMAGE_TAG="v1.0.0"
 echo "start WEB Server"
 
+cd /home/ec2-user/vpc_web
+
 echo "Build image#############################################"
 docker build -t ${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_TAG} .
