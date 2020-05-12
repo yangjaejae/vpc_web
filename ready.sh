b2a75ece34a9095b1f12c59ef41fb4540d30345b
yangjaejae <undertakin@naver.com>
2020-05-12 16:32:23 +0900

    appspec

--- ready.sh
+++ ready.sh
@@ -4,6 +4,8 @@ DOCKER_IMAGE_NAME="web/nodejs"
 DOCKER_IMAGE_TAG="v1.0.0"
 echo "start WEB Server"
 
+cd /home/ec2-user/vpc_web
+
 echo "Ready for start server #############################################"
 docker stop $(docker ps -qa) && docker rm $(docker ps -qa)
 docker rmi -f ${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_TAG}
