e775752ee578b5a65638ee2a656d95406a6be8d4
yangjaejae <undertakin@naver.com>
2020-05-12 16:28:34 +0900

    appspec

---
+++ ready.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+DOCKER_CONTAINER_NAME="web"
+DOCKER_IMAGE_NAME="web/nodejs"
+DOCKER_IMAGE_TAG="v1.0.0"
+echo "start WEB Server"
+
+echo "Ready for start server #############################################"
+docker stop $(docker ps -qa) && docker rm $(docker ps -qa)
+docker rmi -f ${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_TAG}
+docker build -t ${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_TAG} . 
--- stop-docker.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-## application stop 
-pwd
-whoami
-
-echo "Remove container#############################################"
-docker stop $(docker ps -qa) && docker rm $(docker ps -qa)
